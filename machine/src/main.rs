681523a5d8e2b6d357c5e80a3fc1e749e1601030
Terada <terada.yuichiro@monocommunity.com>
2019-02-17 21:13:44 +0900

    refresh screen once per 128 clocks to run faster

--- machine/src/main.rs
+++ machine/src/main.rs
@@ -13,7 +13,6 @@ mod machine;
 mod asm;
 mod window;
 
-
 fn main() {
     let args = env::args().collect::<Vec<_>>();
     if args.len() < 2 {
@@ -21,6 +20,7 @@ fn main() {
         return;
     }
 
+    // read assembly source codes from input file specified with args[1]
     let source = {
         println!("input file is '{}'", args[1]);
         let mut f = std::fs::File::open(&args[1]).expect("cannot open the input file.");
@@ -30,6 +30,7 @@ fn main() {
         source
     };
 
+    // assemble the codes into binary instructions
     let instructions = {
         let instructions = asm::asm(&source).expect("failed to compile asm to binary instructions.");
         println!("*** decoded instructions ***");
@@ -39,11 +40,13 @@ fn main() {
         instructions
     };
 
+    // construct a machine with the instructions
     let mut machine = machine::Machine::new(&instructions);
 
+    // start events loop
     let mut events_loop = glutin::EventsLoop::new();
     let window = window::Window::new(&events_loop);
-
+    let mut counter = 0;
     let mut running = true;
     while running {
         events_loop.poll_events(|event| {
@@ -56,7 +59,12 @@ fn main() {
                 _ => ()
             }
         });
-        window.draw(machine.screen().raw_image());
+
+        // send a clock signal to the machine
         machine.clock(false);
+
+        // refresh screen
+        if counter % 128 == 0 { window.draw(machine.screen().raw_image()); }
+        counter = counter + 1;
     }
 }
